bafe87501e451e26ca5aab345cac729b506d36a5
tsani31 <tsaniii.18@gmail.com>
2025-05-31 01:22:59 +0700

    update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ try {
 const app = express();
 const corsOptions = {
   origin: [
-    'https://fe-notes-115-dot-a-07-451003.uc.r.appspot.com]',
+    'https://fe-notes-115-dot-a-07-451003.uc.r.appspot.com',
     "http://localhost:3000"
   ],
   credentials: true,
