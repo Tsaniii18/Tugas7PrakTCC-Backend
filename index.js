6f899b9c4931c87b219b70799f4410ed7b70235b
tsani31 <tsaniii.18@gmail.com>
2025-06-01 00:43:24 +0700

    bismillah done

--- index.js
+++ index.js
@@ -9,25 +9,20 @@ import dotenv from "dotenv";
 dotenv.config(); 
 
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 
-const allowedOrigins = [
-  "https://fe-notes-115-dot-a-07-451003.uc.r.appspot.com",
-  "http://localhost:3000"
-];
+const corsOptions = {
+ origin: [
+    "https://fe-notes-115-dot-a-07-451003.uc.r.appspot.com",
+    "http://localhost:3000", 
+  ], 
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-// Middleware CORS
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -43,5 +38,4 @@ try {
 }
 
 app.get("/", (req, res) => res.send("Backend is running!"));
-
-app.listen(PORT, () => console.log(`Server up and running on port ${PORT}...`));
+app.listen(PORT, () => console.log('Server up and running....'));
