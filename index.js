8ac262480739d1ec3d89dc201f8051ef8ee0e01f
tsani31 <tsaniii.18@gmail.com>
2025-06-01 12:08:34 +0700

    update index.js

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: '*',
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
