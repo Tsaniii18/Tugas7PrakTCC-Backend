f677f15590a606869ab74f92c19e7b7e9e2a4918
tsani31 <tsaniii.18@gmail.com>
2025-06-01 19:14:39 +0700

    alhamdullilah done

--- index.js
+++ index.js
@@ -34,17 +34,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", req.headers.origin || "");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-    return res.sendStatus(200);
-  }
-  next();
-});
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(route);
