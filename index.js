1364db8d49eb9d7992555d0ffed9c5dc2a9cfdcd
tsani31 <tsaniii.18@gmail.com>
2025-06-01 12:17:56 +0700

    update index.js

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
